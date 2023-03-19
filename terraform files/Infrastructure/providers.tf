4b24417e6b60f1d8b4ab06d84b4a348c822db373
Adeshile Oluwatosin <105433596+TheTosin@users.noreply.github.com>
2023-03-19 22:43:00 +0100

    Update providers.tf

--- terraform files/Infrastructure/providers.tf
+++ terraform files/Infrastructure/providers.tf
@@ -4,14 +4,56 @@
 
 provider "aws" {
   region     = "us-east-1"
+   access_key = "AKIA4YJOBHOEUUN6OPZQ"
+   secret_key = "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"
 }
+
+# Kubectl Terraform provider
 
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.59.0 "
+    kubectl = {
+      source = "gavinbunney/kubectl"
+      version = "1.14.0"
     }
+  }
+}
+
+# Terraform Cloud Eks Workspace
+
+ terraform {
+   cloud {
+     organization = "oluwatosin"
+
+     workspaces {
+       name = "infrastructure"
+     }
+   }
+ }
+
+# Kubernetes provider configuration
+
+provider "kubernetes" {
+  host                   = aws_Altschool_cluster.Altschool-cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_Altschool_cluster.Altschool-cluster.certificate_authority[0].data)
+  version          = "2.16.1"
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", aws_Altschool_cluster.Altschool-cluster.name]
+    command     = "aws"
+  }
+}
+
+# Kubectl provider configuration
+
+provider "kubectl" {
+  host                   = aws_eks_cluster.eks-cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_Altschool_cluster.Altschool-cluster.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", aws_Altschool_cluster.Altschool-cluster.name]
+    command     = "aws"
   }
 }
 
