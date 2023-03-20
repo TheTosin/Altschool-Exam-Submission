f57a404f17a22211daf63ac861ba60b8243bc4d3
Adeshile Oluwatosin <105433596+TheTosin@users.noreply.github.com>
2023-03-20 04:25:36 +0100

    Create eksdomain.tf

---
+++ terraform files/deployment/eksdomain.tf
@@ -0,0 +1,66 @@
+  GNU nano 4.8                                           eksdomain.tf                                                      resource "kubernetes_service" "kube-service-userprofile" {
+  metadata {
+    name = "kube-service-userprofile"
+  }
+
+  spec {
+    selector = {
+      app = "userprofile"
+    }
+
+    port {
+      name       = "http"
+      port       = 80
+      target_port = "http"
+    }
+
+    type = "LoadBalancer"
+  }
+}
+
+
+# Route 53 and sub-domain name setup
+
+resource "aws_route53_zone" "userprofile-domain-name" {
+  name = "userprofile.adeshiletosin.live"
+}
+
+resource "aws_route53_zone" "socks-domain-name" {
+  name = "socks.adeshiletosin.live"
+}
+
+# Get the zone_id for the load balancer
+data "aws_elb_hosted_zone_id" "elb_zone_id" {
+  depends_on = [
+    kubernetes_service.kube-service-userprofile, kubernetes_service.kube-service-socks
+  ]
+}
+
+# DNS record for portfolio
+
+resource "aws_route53_record" "userprofile-record" {
+  zone_id = aws_route53_zone.userprofile-domain-name.zone_id
+  name    = "userprofile.adeshiletosin.live"
+  type    = "A"
+
+  alias {
+    name                   = kubernetes_service.kube-service-portfolio.status.0.load_balancer.0.ingress.0.hostname
+    zone_id                = data.aws_elb_hosted_zone_id.elb_zone_id.id
+    evaluate_target_health = true
+  }
+}
+
+# DNS record for socks
+
+resource "aws_route53_record" "socks-record" {
+  zone_id = aws_route53_zone.socks-domain-name.zone_id
+  name    = "socks.adeshiletosin.live"
+  type    = "A"
+
+  alias {
+    name                   = kubernetes_service.kube-service-socks.status.0.load_balancer.0.ingress.0.hostname
+    zone_id                = data.aws_elb_hosted_zone_id.elb_zone_id.id
+    evaluate_target_health = true
+
+  }
+}
