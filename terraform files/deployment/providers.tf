95e15a9cdffee92d8df2286f96143d143d3907d0
Adeshile Oluwatosin <105433596+TheTosin@users.noreply.github.com>
2023-03-20 19:17:26 +0100

    Update providers.tf

--- terraform files/deployment/providers.tf
+++ terraform files/deployment/providers.tf
@@ -33,6 +33,12 @@ terraform {
 # Kubernetes provider configuration
 
 # Retrieve eks cluster using data source
+# Not required: currently used in conjuction with using
+# icanhazip.com to determine local workstation external IP
+# to open EC2 Security Group access to the Kubernetes cluster.
+# See workstation-external-ip.tf for additional information.
+provider "http" {}
+
 
 data "aws_eks_cluster" "eks-cluster" {
   name = "eks-cluster"
